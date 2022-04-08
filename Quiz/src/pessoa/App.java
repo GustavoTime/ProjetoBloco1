bf804782ed2d8c2d00ed8e2ae5db7814fb2b856b
zRafael <rafael.silva@bandtec.com.br>
2022-04-08 16:35:51 -0300

    Adicionando classe Pessoa

---
+++ Quiz/src/pessoa/App.java
@@ -0,0 +1,12 @@
+package pessoa;
+
+public class App {
+
+	public static void main(String[] args) {
+		
+		Pessoa pessoa1 = new Pessoa("Jogador1", "Iniciante");
+		
+		pessoa1.escolherJogador("Cesar");
+		pessoa1.escolherApresentador("Anderson");
+	}
+}
