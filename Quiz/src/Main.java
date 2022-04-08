aad9f0cfbd262305befa96cf01d4d776ef0b373a
Gustavo Aca <galasdalas50@gmail.com>
2022-04-07 21:25:04 -0300

    MenuFinal iniciado

--- Quiz/src/Main.java
+++ Quiz/src/Main.java
@@ -1,17 +1,11 @@
-import Menu.*;
-import java.util.Scanner;
+
+
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		MenuFim mf = new MenuFim();
 		
-		Scanner ler = new Scanner(System.in);
-		
-		System.out.println("Insira um valor");
-		int n = ler.nextInt();
-		
-		mf.ContAcertosErros(n, n);
 		
 	}
 
--- Quiz/src/Menu/MenuFim.java
+++ Quiz/src/Menu/MenuFim.java
@@ -4,20 +4,40 @@ public class MenuFim {
 	private int contAcerto;
 	private int contErro;
 	private String texto;
+	private int totalPergunta;
 
 	public void RespostaDetalhada() {
 		System.out.println("Correto");
 	}
 
+	// Alterar para receber a resposta do usuario e a lista de resposta certa
 	public void ContAcertosErros(int lit, int certo) {
 
+		// Se a resposta for igual ao gabarito add 1 nos acerto
 		if (lit == certo) {
 			this.setContAcerto(this.getContAcerto() + 1);
+			
+			//Se não add 1 ao erro 
 		} else {
+			
 			this.setContErro(this.getContErro() + 1);
 		}
-		
+
 		System.out.println("contAcertos: " + this.getContAcerto() + "\nContErros: " + this.getContErro());
+	}
+
+	public int PorcentualAcerto() {
+		int porcent = 0;
+		// Soma o numero de perguntas acertadas e erradas para saber o numero de
+		// perguntas total
+		this.setTotalPergunta(this.getContAcerto() + this.getContErro());
+
+		// O porcentual é o numero de perguntas certas * 100 dividido pelo numero de
+		// perguntas total
+		porcent = (this.getContAcerto() * 100) / this.getTotalPergunta();
+
+		// Retorna o valor
+		return porcent;
 	}
 
 	// Metodos auxilaires e mutantes
@@ -43,6 +63,14 @@ public class MenuFim {
 
 	public void setTexto(String texto) {
 		this.texto = texto;
+	}
+
+	public int getTotalPergunta() {
+		return totalPergunta;
+	}
+
+	public void setTotalPergunta(int totalPergunta) {
+		this.totalPergunta = totalPergunta;
 	}
 
 }
